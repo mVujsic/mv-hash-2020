8f647bec2b1a42d1b2b76c69d49366adc394d018
M Vujsic <49861840+mVujsic@users.noreply.github.com>
2020-07-09 15:27:18 +0200

    Add files via upload

---
+++ ploting.py
@@ -0,0 +1,24 @@
+import string
+import random
+import hashlib
+import timeit
+import matplotlib.pylab as plt
+
+def randomly(size, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+dictOF=dict()
+
+for i in range(512,102400,512):
+    random_str = randomly(i)
+    dictOF.update({i:timeit.timeit('hashlib.sha1(random_str).hexdigest()',
+                        setup='import hashlib; random_str="%s".encode("utf-8")' % random_str,
+                        number=200)})
+
+
+lists = sorted(dictOF.items()) 
+
+x, y = zip(*lists) 
+
+plt.plot(x, y)
+plt.show()
